3c2a1195747f67e10a4d85cceb956a75584bee75
Thomas Niels Berthelsen (tnb) <tnb@novicell.dk>
2022-10-04 13:40:04 +0200

    qwefqwefqwef

--- shopping-list-et.api/shopping-list-et.application/ShoppingListModifyName/ShoppingListModifyNameCommand.cs
+++ shopping-list-et.api/shopping-list-et.application/ShoppingListModifyName/ShoppingListModifyNameCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace shopping_list_et.application.ShoppingListModifyName
 {
-    public class ShoppingListModifyNameCommand
+    public class ShoppingListModifyNameCommand : IRequest
     {
         public int ShoppingListId { get; set; }
         public string? ShoppingListName { get; set; }
