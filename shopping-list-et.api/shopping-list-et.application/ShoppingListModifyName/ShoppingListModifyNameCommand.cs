548f77fd7580fa4e98788b3aefb645129670fb1f
Thomas Niels Berthelsen (tnb) <tnb@novicell.dk>
2022-10-04 13:25:20 +0200

    qwefqwefwqef

---
+++ shopping-list-et.api/shopping-list-et.application/ShoppingListModifyName/ShoppingListModifyNameCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopping_list_et.application.ShoppingListModifyName
+{
+    public class ShoppingListModifyNameCommand
+    {
+        public int ShoppingListId { get; set; }
+        public string? ShoppingListName { get; set; }
+    }
+}
---
+++ shopping-list-et.api/shopping-list-et.application/ShoppingListModifyName/ShoppingListModifyNameCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using shopping_list_et.infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopping_list_et.application.ShoppingListModifyName
+{
+    public class ShoppingListModifyNameCommandHandler : IRequestHandler<ShoppingListModifyNameCommand>
+    {
+        private readonly ApplicationDbContext context;
+
+        public ShoppingListModifyNameCommandHandler(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(ShoppingListModifyNameCommand request, CancellationToken cancellationToken)
+        {
+            var shoppingList = await context.ShoppingLists.FindAsync(request.ShoppingListId);
+
+            if (shoppingList is null)
+                throw new ArgumentNullException(nameof(shoppingList));
+
+            shoppingList.Name = request.ShoppingListName;
+
+            await context.SaveChangesAsync();
+
+            return new Unit();
+        }
+    }
+}
